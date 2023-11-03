0106837893d9c2820857ee7920ff1d5b7d97602f
Esteak Shapin <esteakshapin@gmail.com>
2023-11-03 15:42:16 -0400

    added phpFormStart folder

---
+++ dbCon.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "eks3146";
+$password = "Magnesite3#menstruating";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+?>
