d5dd93a8845c266048e61640a385480216330b63
Esteak Shapin <esteakshapin@gmail.com>
2023-11-30 17:12:55 -0500

    added ex09 folder

---
+++ EX09/EX09.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EX 09</title>
+</head>
+<body>
+    <!-- Including the DB connection partial to connect to the database -->
+    <?php include('./dbCon.php') ?>
+    <h1>What do you think?</h1>
+</body>
+</html>
