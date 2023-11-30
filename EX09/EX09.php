c8a333ea6240babf8425f341efdfaf6adf52c739
Esteak Shapin <esteakshapin@gmail.com>
2023-11-30 18:08:30 -0500

    displaying comments from databse

--- EX09/EX09.php
+++ EX09/EX09.php
@@ -7,32 +7,25 @@
 </head>
 <body>
     <!-- Including the DB connection partial to connect to the database -->
-    <?php
-
-    include('./dbCon.php');
-    
-    // get all the comments in the database
-    if ($conn){
-        $res = $conn->query('SELECT `from`, `message`, `date` FROM `comments`');
-    
-        if ($res) {
-            $records = []; // Initialize the array
-            while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
-                $records[] = $row;
-            }
-    
-            if (!empty($records)) {
-                // Assuming you want to echo a specific column, for example, 'comment_text'
-                echo $records[0]['from'];
-            } else {
-                echo "No records found.";
-            }
-        } else {
-            echo "Error in query execution: " . mysqli_error($conn);
-        }
-    }
-    
-    ?>
+    <?php include('./dbCon.php'); ?>
     <h1>What do you think?</h1>
+
+    <ul>
+        <?php 
+            // get all the comments in the database
+            if ($conn){
+                $res = $conn->query('SELECT `from`, `message`, `date` FROM `comments`');
+            
+                if ($res) {
+                    while ($row = mysqli_fetch_array($res, MYSQLI_ASSOC)) {
+                        echo '<li> <span style="color: green">' . $row['from'] . ': </span> ' . $row['message'] . ' @ <span style="color: gray">' . $row['date'] . "</li>";
+                    }
+                } else {
+                    echo "Error in query execution: " . mysqli_error($conn);
+                }
+            }
+        ?>
+    </ul>
+    
 </body>
 </html>
