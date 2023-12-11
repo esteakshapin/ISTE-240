b5c6b9f363cf70093da6877d1160412cb1d585d3
Esteak Shapin <esteakshapin@gmail.com>
2023-12-11 18:11:09 -0500

    refactored chittagong to php

--- final/Chittagong.php
+++ final/Chittagong.php
@@ -1,98 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Chittagong - Time of BD!</title>
-        <link rel="icon" type="image/png"
-            href="static/images/Times of BD-logos.jpeg">
-        <link rel="stylesheet" href="./static/css/index.css">
-        <link href="https://fonts.googleapis.com/css?family=Rajdhani|Glegoo"
-            rel="stylesheet">
-    </head>
+    <!-- Header Section -->
+    <?php
+        $header = 'Chittagong - Time of BD!';
+        include './partials/header.php';
+    ?>
     <body>
         <!-- nav bar -->
-        <nav>
-            <img src="./static/images/Times of BD-logos_transparent.png"
-                alt="Times Of BD Logo">
-
-            <ul class="nav-center-items-container">
-                <a href="./index.html">
-                    <li class="nav-items">
-                        <div class="spacing"></div>
-                        Home
-                        <div class="spacing"></div>
-                    </li>
-                </a>
-
-                <li class="nav-items active">
-                    <div class="spacing"></div>
-                    Cities
-                    <div class="spacing"></div>
-                    <ul class="sub-nav-menu">
-                        <a href="./dhaka.html">
-                            <li class="sub-nav-menu-items">Dhaka</li>
-                        </a>
-                        <a href="./Chittagong.html">
-                            <li class="sub-nav-menu-items">Chittagong</li>
-                        </a>
-                        <a href="./Feni.html">
-                            <li class="sub-nav-menu-items">Feni</li>
-                        </a>
-                    </ul>
-                </li>
-
-                <li class="nav-items">
-                    <div class="spacing"></div>
-                    Food
-                    <div class="spacing"></div>
-                    <ul class="sub-nav-menu">
-                        <a href="./appetizer.html">
-                            <li class="sub-nav-menu-items">Appetizer</li>
-                        </a>
-                        <a href="./entree.html">
-                            <li class="sub-nav-menu-items">Entree</li>
-                        </a>
-                        <a href="./desert.html">
-                            <li class="sub-nav-menu-items">Desert</li>
-                        </a>
-                    </ul>
-                </li>
-
-                <li class="nav-items">
-                    <div class="spacing"></div>
-                    Vacation
-                    <div class="spacing"></div>
-                    <ul class="sub-nav-menu">
-                        <a href="./cox-bazar.html">
-                            <li class="sub-nav-menu-items">Cox Bazar</li>
-                        </a>
-                        <a href="./rangamati.html">
-                            <li class="sub-nav-menu-items">Rangamati</li>
-                        </a>
-                        <a href="./st-martin-island.html">
-                            <li class="sub-nav-menu-items">St. Martin's Island</li>
-                        </a>
-                    </ul>
-                </li>
-            </ul>
-
-            <a href="./bibliography.html"><span id="about-me">Work Cited</span></a>
-        </nav>
+        <?php
+            $active = "Cities";
+            include './partials/navbar.php';
+        
+        ?>
         <!-- nav bar -->
 
         <!-- This carousel cycles through a list of images  -->
-        <div class="carousel-container">
-            <div class="carousel">
-                <!-- default image -->
-                <img id="carousel-img"
-                    src="./static/images/Chittagong-carousel/IMG_2062.jpg"
-                    alt="Slide Show of Images Taken From Shapin's Bangladesh Trip in Chittagong">
-            </div>
-            <div class="title-box">
-                <h1>Chittagong</h1>
-            </div>
-        </div>
+        <?php
+            $carouselSrc = "./static/images/Chittagong-carousel/IMG_9525.jpg";
+            $alt = "Slide Show of Images Taken From Shapin's Bangladesh Trip in Chittagong";
+            $carouselTitle = "Chittagong";
+            include './partials/carousel.php';
+        
+        ?>
 
         <!-- main content section -->
         <div class="content">
@@ -173,11 +102,12 @@
         </div>
         <!-- main content section -->
 
+        <!-- footer section -->
+        <?php include "./partials/footer.php" ?>
+
         <script>
             // list of image URL that the carousel uses to cycle through
             const imageUrls = [
-                './static/images/Chittagong-carousel/IMG_2062.jpg',
-                './static/images/Chittagong-carousel/IMG_2078.jpg',
                 './static/images/Chittagong-carousel/IMG_9525.jpg',
                 './static/images/Chittagong-carousel/IMG_9559.jpg',
                 './static/images/Chittagong-carousel/IMG_9561.jpg',
