d1f4696297510031ac7da4afbb400e650bcc59d9
Esteak Shapin <esteakshapin@gmail.com>
2023-12-12 15:11:13 -0500

    added dunamic name to form

--- final/partials/comment_form.php
+++ final/partials/comment_form.php
@@ -1,6 +1,6 @@
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" class="comment-form" method="POST">
 
-    <label for="">Name: <h4>John Doe</h4></label>
+    <label for="">Name: <h4><?php echo $_SESSION['first_name'] . " " . $_SESSION['last_name']; ?></h4></label>
     <br>
     <label for="comment">Leave your comment:</label><br>
     <textarea name="comment" id="comment" cols="50" rows="10" required></textarea>
